37f1042880b13b9dc69f451b8e807e60b17b3269
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2017-04-26 11:34:51 +0200

    updated discount conditions

--- code/forms/DiscountField.php
+++ code/forms/DiscountField.php
@@ -21,89 +21,88 @@ class DiscountField extends TextField
 {
     public function validate($validator)
     {
-        if (isset($this->value) && $this->value) {
+        // If no discount is set continue doing default validation
+        if (!isset($this->value) && !$this->value) {
+            return parent::validate($validator);
+        }
 
-            /** @var Discount $discount */
-            // Check if the discount exists
-            if (!$discount = Discount::get()->find('Code', $this->value)) {
-                $validator->validationError($this->name, _t(
-                    'DiscountField.VALIDATION_NOT_FOUND',
-                    'The entered coupon is not found'
-                ), 'validation');
+        /** @var Discount $discount */
+        // Check if the discount exists
+        if (!$discount = Discount::get()->find('Code', $this->value)) {
+            $validator->validationError($this->name, _t(
+                'DiscountField.VALIDATION_NOT_FOUND',
+                'The entered coupon is not found'
+            ), 'validation');
 
-                return false;
-            }
+            return false;
+        }
 
-            // Check if the discount is already used
-            if ($discount->getUsed()) {
-                $validator->validationError($this->name, _t(
-                    'DiscountField.VALIDATION_USED_CHECK',
-                    'The entered coupon is already used'
-                ), 'validation');
+        // Check if the discount is already used
+        if ($discount->getUsed()) {
+            $validator->validationError($this->name, _t(
+                'DiscountField.VALIDATION_USED_CHECK',
+                'The entered coupon is already used'
+            ), 'validation');
 
-                return false;
-            }
+            return false;
+        }
 
-            /** @var DiscountForm $form */
-            $form = $this->form;
+        /** @var DiscountForm $form */
+        $form = $this->form;
 
-            // Check if the coupon is expired
-            if (!$checkDate = $discount->validateDate()) {
-                $validator->validationError($this->name, _t(
-                    'DiscountField.VALIDATION_DATE_CHECK',
-                    'The coupon is expired'
-                ), 'validation');
+        // Check if the coupon is expired
+        if (!$checkDate = $discount->validateDate()) {
+            $validator->validationError($this->name, _t(
+                'DiscountField.VALIDATION_DATE_CHECK',
+                'The coupon is expired'
+            ), 'validation');
 
-                return false;
-            }
+            return false;
+        }
 
-            // Check if the coupon is allowed on this event
-            if (!$checkEvent = $discount->validateEvents($form->getReservation()->Event())) {
-                $validator->validationError($this->name, _t(
-                    'DiscountField.VALIDATION_EVENT_CHECK',
-                    'The coupon is not allowed on this event'
-                ), 'validation');
+        // Check if the coupon is allowed on this event
+        if (!$checkEvent = $discount->validateEvents($form->getReservation()->Event())) {
+            $validator->validationError($this->name, _t(
+                'DiscountField.VALIDATION_EVENT_CHECK',
+                'The coupon is not allowed on this event'
+            ), 'validation');
 
-                return false;
-            }
+            return false;
+        }
 
-            // If groups are required check if one of the attendees is in the required group
-            if (!$checkMember = $discount->validateGroups()) {
-                foreach ($form->getReservation()->Attendees() as $attendee) {
-                    /** @var Attendee $attendee */
-                    if ($attendee->Member()->exists() && $member = $attendee->Member()) {
-                        if ($checkMember = $discount->validateGroups($member)) {
-                            // If one of the member is part of the group validate the discount
-                            break;
-                        } else {
-                            $checkMember = false;
-                        }
+        // If groups are required check if one of the attendees is in the required group
+        if (!$checkMember = $discount->validateGroups()) {
+            foreach ($form->getReservation()->Attendees() as $attendee) {
+                /** @var Attendee $attendee */
+                if ($attendee->Member()->exists() && $member = $attendee->Member()) {
+                    if ($checkMember = $discount->validateGroups($member)) {
+                        // If one of the member is part of the group validate the discount
+                        break;
+                    } else {
+                        $checkMember = false;
                     }
                 }
             }
-
-            if (!$checkMember) {
-                $validator->validationError($this->name, _t(
-                    'DiscountField.VALIDATION_MEMBER_CHECK',
-                    'None of the attendees is allowed to use this coupon'
-                ), 'validation');
-
-                return false;
-            }
-
-            // If all checks passed add the discount and recalculate the price
-            if ($checkDate && $checkEvent && $checkMember) {
-                $discount->write();
-                $form->getReservation()->PriceModifiers()->add($discount);
-                $form->getReservation()->calculateTotal();
-                $form->getReservation()->write();
-                return true;
-            }
+        }
+        
+        if (!$checkMember) {
+            $validator->validationError($this->name, _t(
+                'DiscountField.VALIDATION_MEMBER_CHECK',
+                'None of the attendees is allowed to use this coupon'
+            ), 'validation');
 
             return false;
-        } else {
-            // If the field is empty continue without adding the discount
-            return parent::validate($validator);
         }
+
+        // If all checks passed add the discount and recalculate the price
+        if ($checkDate && $checkEvent && $checkMember) {
+            $discount->write();
+            $form->getReservation()->PriceModifiers()->add($discount);
+            $form->getReservation()->calculateTotal();
+            $form->getReservation()->write();
+            return true;
+        }
+
+        return false;
     }
 }
